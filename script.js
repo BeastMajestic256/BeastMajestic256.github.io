79d1cc60a33a1d334d781d7510a3b8bab5d9fc8a
BeastMajestic256 <136160619+BeastMajestic256@users.noreply.github.com>
2023-07-06 22:51:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function Submit() {
   alert("Oh, and we not only scammed you, now we have valid credentials for your credit card! We will drain it!")
   alert("Using only HTML, CSS, and Javascript, we managed to fool you :)")
 }
+
+function orderButton(productNum) {
+  var captcha = document.getElementById("captcha").checked;
+  var emailAcc = document.getElementById("emailAcc").value;
+  var homeAddr = document.getElementById("homeAddr").value;
+  var credCardNum = document.getElementById("credCardNum").value;
+
+  if (captcha !== true) {
+    alert("Please check CAPTCHA")
+    return;
+  }
+
+  if (emailAcc === '' ||
+      homeAddr === '' ||
+      credCardNum === '') {
+    alert("One or more fields is missing. Please input the required parameters.")
+    return;
+  }
+  
+  alert("Thank you for ordering!");
+  alert(`Order number: # ${Math.floor(Math.random() * (65535 - 1 + 1)) + 1}`);
+  alert("Packing up rocks...")
+
+  return;
+}
