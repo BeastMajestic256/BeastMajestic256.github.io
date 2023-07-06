0736f57ec164df176bab92bd17b2ca5b4522e77d
BeastMajestic256 <136160619+BeastMajestic256@users.noreply.github.com>
2023-07-06 13:07:49 +0800

    Create script.js
    log :: Create - script.js :: Github;

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Hi there!\nTotally not a scam website :)")
+
+
+function Submit() {
+  alert("Thanks for ordering!")
+  alert("Meanwhile we will not pack your order, instead give you something trash instead.")
+  alert("Oh, and we not only scammed you, now we have valid credentials for your credit card! We will drain it!")
+  alert("Using only HTML, CSS, and Javascript, we managed to fool you :)")
+}
